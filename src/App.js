edb46ed0a91dc872dd4d19a2ebd7802c45f09937
Nima Boscarino <nima.boscarino@gmail.com>
2020-02-10 00:48:32 -0800

    setTimeout for loading pins

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar'
 import PinMasonry from './components/PinMasonry'
 import sampleData from './components/PinMasonry/sample_data.json'
 
 function App() {
+  const [pins, setPins] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchData = () => {
+      setTimeout(() => {
+        setPins(sampleData)
+        setLoading(false)
+      }, 2000) // two seconds to simulate network request
+    }
+
+    setLoading(true)
+    fetchData()
+  }, [])
+
   return (
     <div className={"container"}>
       <SearchBar />
-      <PinMasonry pins={sampleData} />
+      {
+        loading ? <h1>Loading...</h1> : <PinMasonry pins={pins} />
+      }
     </div>
   );
 }
