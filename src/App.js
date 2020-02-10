ddef518ff86abf05679837201081c8627e7f9112
Nima Boscarino <nima.boscarino@gmail.com>
2020-02-10 01:57:51 -0800

    w/ reducers

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar'
 import PinMasonry from './components/PinMasonry'
 import sampleData from './components/PinMasonry/sample_data.json'
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_PINS':
+      return {...state, pins: action.payload, loading: false};
+    case 'SET_FILTERED':
+      return {...state, filteredPins: action.payload};
+    case 'SET_LOADING':
+      return {...state, loading: action.payload};
+    case 'SET_SEARCH':
+      return {...state, search: action.payload};
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
-  const [pins, setPins] = useState([])
-  const [filteredPins, setFilteredPins] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [search, setSearch] = useState("")
+  const [{ pins, filteredPins, loading, search}, dispatch] = useReducer(reducer, {
+    pins: [],
+    filteredPins: [],
+    loading: false,
+    search: ""
+  })
 
   useEffect(() => {
     const fetchData = () => {
       setTimeout(() => {
-        setPins(sampleData)
-        setLoading(false)
+        dispatch({ type: 'SET_PINS', payload: sampleData })
       }, 2000) // two seconds to simulate network request
     }
 
-    setLoading(true)
+    dispatch({ type: 'SET_LOADING', payload: true })
     fetchData()
   }, [])
 
   useEffect(() => {
-    setFilteredPins(
-      pins.filter(pin => pin.title.includes(search) || pin.author.includes(search))
-    )
+    dispatch({
+      type: 'SET_FILTERED',
+      payload: pins.filter(pin => pin.title.includes(search) || pin.author.includes(search))
+    })
   }, [pins, search])
 
   return (
     <div className={"container"}>
-      <SearchBar onChange={setSearch} />
+      <SearchBar onChange={(newSearch) => dispatch({ type: 'SET_SEARCH', payload: newSearch })} />
       {
         loading ? <h1>Loading...</h1> : <PinMasonry pins={filteredPins} />
       }
